05153e01f6460f9bc96d772444a6ecb3b9b58751
dingchao <3440597@qq.com>
2022-07-04 23:48:16 +0800

    fifth commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+        System.out.println( "Hxx" );
+
     }
 }
